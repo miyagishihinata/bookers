73f5984a4cc8dfb805102ea334170ae5e8eafc68
miyagishihinata <117428868+miyagishihinata@users.noreply.github.com>
2022-11-17 03:15:46 +0000

    [Sixth] 3-13

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   get 'books/show'
   get 'books/edit'
   root to: 'homes#top'
-  get '/books' => 'books#new'
+  get '/books' => 'books#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
