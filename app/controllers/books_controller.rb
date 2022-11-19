5f70efd34b6ba6476beef56bfa02501d0a12128e
miyagishihinata <117428868+miyagishihinata@users.noreply.github.com>
2022-11-19 07:36:43 +0000

    [15] 3-13

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,12 +4,13 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    flash[:notice] = "successfully"
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if  @book.save
+    redirect_to book_path(@book.id)
+    else
+      render :index
+    end
   end
-
 
   def index
     @book = Book.new
