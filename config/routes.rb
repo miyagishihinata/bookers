36fb73c0b82b26bbebe63f7228af36999d4b39fc
miyagishihinata <117428868+miyagishihinata@users.noreply.github.com>
2022-11-19 05:51:30 +0000

    [14] 3-13

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get '/books' => 'books#index'
   patch 'books/:id' => 'books#update', as: 'update_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
